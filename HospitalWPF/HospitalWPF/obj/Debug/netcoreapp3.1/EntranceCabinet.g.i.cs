49fca87b8873b200b7274ccfab2266e6fd744c50
akkafe1ix <alex.from2018@yandex.ru>
2023-02-27 22:44:48 +0300

    Kakashki2

--- HospitalWPF/HospitalWPF/obj/Debug/netcoreapp3.1/EntranceCabinet.g.i.cs
+++ HospitalWPF/HospitalWPF/obj/Debug/netcoreapp3.1/EntranceCabinet.g.i.cs
@@ -66,13 +66,13 @@ namespace HospitalWPF {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.15.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/HospitalWPF;component/entrancecabinet.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/HospitalWPF;V1.0.0.0;component/entrancecabinet.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\EntranceCabinet.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -82,7 +82,7 @@ namespace HospitalWPF {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.15.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
--- HospitalWPF/HospitalWPF/obj/Debug/netcoreapp3.1/PersonalAccount.g.i.cs
+++ HospitalWPF/HospitalWPF/obj/Debug/netcoreapp3.1/PersonalAccount.g.i.cs
@@ -50,13 +50,13 @@ namespace HospitalWPF {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.15.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/HospitalWPF;component/personalaccount.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/HospitalWPF;V1.0.0.0;component/personalaccount.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\PersonalAccount.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -66,7 +66,7 @@ namespace HospitalWPF {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.15.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
