c33514c7120a703659a933ec4ce4665afe872662
FrizyClop <wifikin@mail.ru>
2023-03-06 23:21:31 +0300

    сейчас всё сломается

--- HospitalWPF/HospitalWPF/obj/Debug/netcoreapp3.1/App.g.i.cs
+++ HospitalWPF/HospitalWPF/obj/Debug/netcoreapp3.1/App.g.i.cs
@@ -47,7 +47,7 @@ namespace HospitalWPF {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -59,7 +59,7 @@ namespace HospitalWPF {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/HospitalWPF;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/HospitalWPF;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -73,7 +73,7 @@ namespace HospitalWPF {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
         public static void Main() {
             HospitalWPF.App app = new HospitalWPF.App();
             app.InitializeComponent();
